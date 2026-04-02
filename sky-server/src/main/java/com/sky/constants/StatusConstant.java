70b6b0766aeff2d51f3a214bbf70563f27d91032
0x3f3 <3234378840@qq.com>
2026-04-02 14:15:33 +0800

    初始化苍穹外卖项目

---
+++ sky-server/src/main/java/com/sky/constants/StatusConstant.java
@@ -0,0 +1,8 @@
+package com.sky.constants;
+
+public class StatusConstant {
+    //启用
+    public static final Integer ENABLE = 1;
+    //禁止
+    public static final Integer DISABLE = 0;
+}
--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +15,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     /**
-     * 捕获业务异常
+     * 处理SQL异常
      * @param ex
      * @return
      */
     @ExceptionHandler
     public Result exceptionHandler(BaseException ex){
+        //Duplicate entry 'zhangsan' for key 'employee.idx_username'
         log.error("异常信息：{}", ex.getMessage());
-        return Result.error(ex.getMessage());
+        String message = ex.getMessage();// 1. 获取异常的错误信息
+        if(message.contains("Duplicate entry")){
+            String[] split = message.split(" ");
+            String username = split[2];
+            String msg = username+ MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
